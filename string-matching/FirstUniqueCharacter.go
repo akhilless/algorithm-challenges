f77e253998402e06cf1c2f739ebc27190f64ad82
akhilless <32060794+akhilless@users.noreply.github.com>
2025-10-03 12:49:37 +0200

    Create FirstUniqueCharacter.go

---
+++ string-matching/FirstUniqueCharacter.go
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/first-unique-character-in-a-string/description/
+class FirstUniqueCharacter {
+    public int firstUniqChar(String s) {        
+        int[] chars = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            chars[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (chars[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;        
+    }
+}
